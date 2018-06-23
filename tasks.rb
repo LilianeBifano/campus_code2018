ef7140f99b4fc4c1e6c418f3fa53e97ad6056097
Liliane Bifano <bifano.liliane@gmail.com>
2018-06-23 09:56:17 -0700

    Atividade com orientacao ao objeto

--- tasks.rb
+++ tasks.rb
@@ -1,5 +1,5 @@
-# mensagem de boas vindas
-puts 'Bem vindo ao Task List!'
+require_relative 'task'
+puts('Bem vindo ao Task List!')
 
 INSERIR_TAREFA = 1
 VER_TAREFA = 2
@@ -7,14 +7,13 @@ BUSCAR_TAREFA = 3
 MARCAR_FEITA = 4
 SAIR = 0
 
-#exibir opcoes para o usuario
 def menu
   puts
   puts '### MENU ###'
   puts 'Escolha uma opcao:'
   puts "##{INSERIR_TAREFA} - Inserir uma tarefa"
   puts "##{VER_TAREFA} - Ver todas as tarefas"
-  puts "##{BUSCAR_TAREFA} - Buscar uma tarefa" #receber um texto e procurar essa tarefa na lista
+  puts "##{BUSCAR_TAREFA} - Buscar uma tarefa"
   puts "##{MARCAR_FEITA} - Mudar o status de uma tarefa"
   puts "##{SAIR} - Sair"
   puts
@@ -22,13 +21,9 @@ def menu
   gets().to_i()
 end
 
-def imprime_tarefa(task)
-  puts "Tarefa #{task[:id]} >> #{task[:descricao]} || Feita: #{task[:feita]}"
-end
-
-def imprime_tarefaID(tasks)
+def imprime_tarefas(tasks)
   tasks.each do |t|
-  puts imprime_tarefa(t)
+  puts t
 end
 end
 
@@ -39,51 +34,44 @@ tarefas = []
 while (opcao_escolhida != SAIR ) do
 
   if opcao_escolhida == INSERIR_TAREFA
-    #codigo para inserir uma tarefa
     print 'Inserir uma tarefa: '
     descricao_tarefa = gets.chomp
 
-    tarefa = {id: tarefa_id, descricao: descricao_tarefa, feita: false}
-
+    tarefa = Task.new(tarefa_id, descricao_tarefa)
     tarefas << tarefa
 
-    puts "Tarefa incluida: #{descricao_tarefa}" #interpolar strings
+    puts "Tarefa incluida: #{descricao_tarefa}"
 
     tarefa_id = tarefa_id + 1
 
   elsif opcao_escolhida == VER_TAREFA
-    tarefas.each do |tarefa|
-    imprime_tarefa(tarefa)
-  end
+    imprime_tarefas(tarefas)
+
   elsif opcao_escolhida == BUSCAR_TAREFA
     print'Insira o texto que voce deseja buscar: '
     texto_busca = gets().chomp().upcase()
-
+#Porque nao esta dando mais certo criar um array para as tarefas_encontradas?
     tarefas_encontradas = []
 
     tarefas_encontradas = tarefas.select do |tarefa|
-      tarefa[:descricao].upcase.include? texto_busca
-      end
+      tarefas[:descricao].upcase.include? texto_busca
+  end
 
-        puts "#{tarefas_encontradas.length} tarefas entcontradas"
-        imprime_tarefaID(tarefas_encontradas)
+    puts "#{tarefas_encontrada.length} tarefas entcontradas"
+    puts t(tarefas_encontradas)
 
   elsif opcao_escolhida == MARCAR_FEITA
-    #1- Imorimir todas as tarefas, exibindo o ID
+  #eu nao estou conseguindo desenvolver as mudancas sem orientacoes/resolucoes, acredito que nao estou conseguindo capturar os conceitos, vou tentar desenvolver nas lives e depois retornar.
     puts('Escolha qual tarefa deseja marcar como feita:')
     puts
     imprime_tarefaID(tarefas)
-    #2- Perguntar qual tarefa quer marcar como feita (usando ID)
     print('Qual tarefa deseja marcar como feita?')
     id_marcar_feita = gets().to_i()
-    #3 - Buscar a tarefa pelo ID e trocar o valor de fita para true
     tarefas.each do |t|
       if t[:id] == id_marcar_feita
         t [:feita] = true
       end
 
-    #4 - Atualizar a tarefa no array (ex remover a tarefa do array e reinserir com valor atualizado)
-    #5 - Imprimir uma mensagem de sucesso
   end
   else
     puts 'Opcao invalida'
